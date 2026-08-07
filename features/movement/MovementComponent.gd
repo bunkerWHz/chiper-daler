6362e0644cfbf1c4f69eed445f4276af513fae47
Andrei <bunkerWHz@users.noreply.github.com>
2026-08-07 16:17:56 +0500

    TASK-029

--- features/movement/MovementComponent.gd
+++ features/movement/MovementComponent.gd
@@ -8,12 +8,39 @@ func _ready() -> void:
 	var body_component := actor.get_component(CharacterBodyComponent) as CharacterBodyComponent
 	body = body_component.get_body()
 
+func _update_horizontal_velocity(direction: float, delta: float) -> void:
+	var target_speed := direction * config.move_speed
+
+	match config.acceleration_mode:
+		MovementConfig.AccelerationMode.INSTANT:
+			body.velocity.x = target_speed
+
+		MovementConfig.AccelerationMode.SMOOTH:
+			if direction != 0:
+				body.velocity.x = move_toward(
+					body.velocity.x,
+					target_speed,
+					config.acceleration * delta
+				)
+			else:
+				body.velocity.x = move_toward(
+					body.velocity.x,
+					0.0,
+					config.deceleration * delta
+				)
+
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_axis("move_left", "move_right")
-	body.velocity.x = direction * config.move_speed
+	var direction := Input.get_axis("move_left", "move_right")
+
+	_update_horizontal_velocity(direction, delta)
+
 	if Input.is_action_just_pressed("jump") and body.is_on_floor():
 		body.velocity.y = -config.jump_velocity
+
 	if not body.is_on_floor():
 		body.velocity.y += config.gravity * delta
+
 	body.move_and_slide()
+	
 	print(body.velocity.y)
+	print(body.velocity.x)
--- features/movement/MovementConfig.gd
+++ features/movement/MovementConfig.gd
@@ -4,3 +4,12 @@ class_name MovementConfig
 @export var move_speed: float = 200.0
 @export var gravity: float = 1200.0
 @export var jump_velocity: float = 450.0
+
+@export var acceleration_mode: AccelerationMode = AccelerationMode.SMOOTH
+@export var acceleration: float = 1200.0
+@export var deceleration: float = 1500.0
+
+enum AccelerationMode {
+	INSTANT,
+	SMOOTH
+}
