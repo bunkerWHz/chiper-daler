8be8037a60757fe0e0381e48b95d3f411d6d34e9
Andrei <bunkerWHz@users.noreply.github.com>
2026-08-07 14:55:08 +0500

    TASK-021

--- features/movement/MovementComponent.gd
+++ features/movement/MovementComponent.gd
@@ -1,2 +1,6 @@
-extends Node2D
+extends Component
 class_name MovementComponent
+
+func _ready() -> void:
+	var body_component := actor.get_component(CharacterBodyComponent) as CharacterBodyComponent
+	print(body_component.get_body())
--- framework/components/CharacterBodyComponent.gd
+++ framework/components/CharacterBodyComponent.gd
@@ -1,2 +1,7 @@
 extends Component
 class_name CharacterBodyComponent
+
+const BODY_NODE = "CharacterBody2D"
+
+func get_body() -> CharacterBody2D: 
+	return actor.get_component(CharacterBodyComponent).find_child(BODY_NODE)
