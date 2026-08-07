72d7ba9ec0a9350c43c9621d91231acfe17e9ed5
Andrei <bunkerWHz@users.noreply.github.com>
2026-08-07 15:49:49 +0500

    TASK-026

--- features/movement/MovementComponent.gd
+++ features/movement/MovementComponent.gd
@@ -4,11 +4,11 @@ class_name MovementComponent
 
 var body: CharacterBody2D
 
-func on_initialize() -> void:
+func _ready() -> void:
 	var body_component := actor.get_component(CharacterBodyComponent) as CharacterBodyComponent
 	body = body_component.get_body()
 
-func _physics_process(delta: float) -> void:
-	var direction = Input.get_axis("ui_left", "ui_right")
+func _physics_process(_delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right")
 	body.velocity.x = direction * config.move_speed
 	body.move_and_slide()
