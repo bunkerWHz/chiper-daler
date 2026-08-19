884ff5be13cdc89b4fd1f9c795082d5d22fd006d
Andrei <bunkerWHz@users.noreply.github.com>
2026-08-19 14:56:58 +0500

    TASK-061

--- framework/components/InteractionPromptComponent.gd
+++ framework/components/InteractionPromptComponent.gd
@@ -1,12 +1,27 @@
 extends Component
 class_name InteractionPromptComponent
 
-@export var prompt: Label
-
+var prompt: Label
+var camera: Camera2D
 var interaction_component: InteractionComponent
 
 
 func _ready() -> void:
+
+	var camera_component: CameraComponent = (
+	actor.get_component(CameraComponent)
+	as CameraComponent
+	)
+
+	if camera_component == null:
+		push_error("InteractionPromptComponent requires CameraComponent")
+		return
+
+	camera = camera_component.camera
+
+	if camera == null:
+		push_error("InteractionPromptComponent requires Camera2D")
+		return
 	interaction_component = (
 		actor.get_component(InteractionComponent)
 		as InteractionComponent
@@ -16,15 +31,11 @@ func _ready() -> void:
 		push_error("InteractionPromptComponent requires InteractionComponent")
 		return
 
-	if prompt == null:
-		push_error("InteractionPromptComponent requires Label")
-		return
-
-	prompt.visible = false
-
 
 func _process(_delta: float) -> void:
 	if interaction_component == null:
+		return
+	if not _find_prompt():
 		return
 
 	var target := interaction_component.get_target()
@@ -35,3 +46,17 @@ func _process(_delta: float) -> void:
 
 	prompt.visible = true
 	prompt.text = target.interaction_name
+	var world_position := target.actor.global_position
+	prompt.position = camera.get_screen_center_position() + (
+		world_position - camera.global_position )
+
+
+func _find_prompt() -> bool:
+	if prompt != null:
+		return true
+
+	prompt = get_tree().get_first_node_in_group(
+		"interaction_prompt"
+	) as Label
+
+	return prompt != null
